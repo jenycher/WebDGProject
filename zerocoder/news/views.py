730493f8d0f4797e21ff618720dd60ef9964bbf6
jenycher <168509289+jenycher@users.noreply.github.com>
2024-07-17 22:52:54 +0300

    sum

--- zerocoder/news/views.py
+++ zerocoder/news/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import News_post
+from .forms import News_postForm
 
 # Create your views here.
 def home(request):
@@ -7,4 +8,13 @@ def home(request):
     return render(request, 'news/news.html', {'news': news})
 
 def create_news(request):
-     return render(request, 'news/add_new_post.html')
+    error=""
+    if request.method == 'POST':
+        form = News_postForm(request.POST)  # Сюда сохранится информация от пользователя.
+        if form.is_valid():
+                form.save()
+                return redirect('news_home')
+        else:
+            error = "Данные были заполнены некорректно"
+    form = News_postForm()
+    return render(request, 'news/add_new_post.html', {'form': form, 'error': error})
